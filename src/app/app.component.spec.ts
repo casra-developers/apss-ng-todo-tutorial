dfd9d45cc6e40a71432d4a239334508bae151a71
Felix Schmorell <felix.schmorell@casra.ch>
2023-08-15 15:23:21 +0200

    changing html for root component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-app app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Todo List');
   });
 });
