abb36630229ec44b7be57b038ef055eb79ffa35c
Felix Schmorell <felix.schmorell@casra.ch>
2023-08-15 15:42:20 +0200

    adding todo card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoCardComponent } from './todo-card/todo-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoCardComponent
   ],
   imports: [
     BrowserModule
