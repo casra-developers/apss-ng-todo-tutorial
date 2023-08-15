df9526ebcf8445b30869cb8f5a8cd664f3cbf0a4
Felix Schmorell <felix.schmorell@casra.ch>
2023-08-15 15:49:08 +0200

    adding todo model and array

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../models/todo';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
   title = 'Todo List';
+
+  todos: Todo[];
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First task',
+        completed: false
+      },
+      {
+        content: 'Second task',
+        completed: true
+      }
+    ];
+  }
+
 
 }
